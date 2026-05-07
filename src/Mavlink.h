580d6e39090ffaf886144dad5a0221e1beb48bff
pschatzmann <phil.schatzmann@gmail.com>
2026-05-07 09:31:12 +0200

    Move MavlinkConfig.h to root

--- src/Mavlink.h
+++ src/Mavlink.h
@@ -3,6 +3,7 @@
 // Copyright (c) 2026 pschatzmann
 
 #pragma once
+#include "MavlinkConfig.h"
 #include "Mavlink/MavlinkController.h"
 #include "Mavlink/ParameterStore.h"
 #include "Mavlink/ParameterMapStore.h"
